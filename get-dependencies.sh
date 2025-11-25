a6df36c7a4c33d88b007760730b0ae0c5f2ce074
fiftydinar <65243233+fiftydinar@users.noreply.github.com>
2025-11-25 12:14:04 +0100

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -14,8 +14,8 @@ git clone https://gitlab.archlinux.org/archlinux/packaging/packages/cartridges.g
 	cd ./cartridges
 	sed -i -e "s|pkgver=*|pkgver=$VER|" ./PKGBUILD
     sed -i -e "s|pkgrel=*|pkgrel=1|" ./PKGBUILD
-	sed -i '/^b2sums/,/^)/d' ./PKGBUILD
-	echo -e "\nb2sums=(\n  'SKIP'\n'  SKIP\n)" >> ./PKGBUILD
+    sed -i -e "s|e15ee8046903a4b0307af88ab79c5dc951bfc69ea8a76109fec4690f95ddf6eec8cef2c2d083444e00520493d7e6d2f4c165e2dafcdaa026b4784cfd7bd1fc90|'SKIP'|" ./PKGBUILD
+    sed -i -e "s|d0f7ec28c561cc3d6b92943884bc6c467fac642d0f4950e0954061f027d4ecac691436dc8e4d9f863fa7318a9007eafda94cde30b72cf8e8871dfa154b97e299|'SKIP'|" ./PKGBUILD
     makepkg -fs --noconfirm
 	ls -la .
 	pacman --noconfirm -U *.pkg.tar.*
