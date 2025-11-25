b689842720b80e17645059d215cb3499a240bfb9
fiftydinar <65243233+fiftydinar@users.noreply.github.com>
2025-11-25 11:31:11 +0100

    Initial install of dependencies

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -4,15 +4,20 @@ set -eu
 
 ARCH=$(uname -m)
 
-echo "Installing package dependencies..."
+echo "Building package and its dependencies..."
 echo "---------------------------------------------------------------"
-# pacman -Syu --noconfirm PACKAGESHERE
+wget https://codeberg.org/kramo/cartridges/raw/branch/main/meson.build -O /tmp/meson.build
+VER="$(meson introspect /tmp/meson.build --projectinfo | jq -r '.version')"
+sed -i 's|EUID == 0|EUID == 69|g' /usr/bin/makepkg
+git clone https://gitlab.archlinux.org/archlinux/packaging/packages/cartridges.git ./cartridges && (
+	cd ./cartridges
+	sed -i -e "s|pkgver=*|pkgver=$VER|" ./PKGBUILD
+  sed -i -e "s|pkgrel=*|pkgrel=1|" ./PKGBUILD
+  makepkg -fs --noconfirm
+	ls -la .
+	pacman --noconfirm -U *.pkg.tar.*
+)
 
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
 get-debloated-pkgs --add-common --prefer-nano
-
-# Comment this out if you need an AUR package
-#make-aur-package PACKAGENAME
-
-# If the application needs to be manually built that has to be done down here
